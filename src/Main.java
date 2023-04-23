386242b704f72c8fc3232c55b59a50b1a29fc4b5
andredw13L <andreribeiro13l@gmail.com>
2023-04-23 03:02:40 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -11,7 +10,7 @@ public class Main {
         curso1.setDescricao("Aprenda a linguagem Java");
         curso1.setCargaHoraria(8);
 
-        System.out.println("\n" + curso1);
+//        System.out.println("\n" + curso1);
 
 
         Curso curso2 = new Curso();
@@ -20,7 +19,7 @@ public class Main {
         curso2.setDescricao("Aprenda JavaScript");
         curso2.setCargaHoraria(10);
 
-        System.out.println("\n" + curso2);
+//        System.out.println("\n" + curso2);
 
 
         Mentoria mentoria = new Mentoria();
@@ -29,6 +28,45 @@ public class Main {
         mentoria.setDescricao("Mentoria de Java POO");
         mentoria.setData(LocalDate.now());
 
-        System.out.println("\n" + mentoria);
+//        System.out.println("\n" + mentoria);
+
+
+        // Polimorfismo: A partir da classe mãe, vamos instanciar a classe filha
+//        Conteudo conteudo = new Curso();
+
+
+        Bootcamp bootcamp = new Bootcamp();
+
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAndre = new Dev();
+
+        devAndre.setNome("André");
+        devAndre.inscreverBootcamp(bootcamp);
+        System.out.println("\nConteúdos inscritos: " + devAndre.getConteudosInscritos());
+        devAndre.progredir();
+        devAndre.progredir();
+        devAndre.progredir();
+        System.out.println("\nConteúdos inscritos: " + devAndre.getConteudosInscritos());
+        System.out.println("\nConteúdos concluídos: " + devAndre.getConteudosConcluidos());
+        System.out.println("\nXP: " + devAndre.calcularTotalXP());
+
+
+        Dev devJoao = new Dev();
+
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("\nConteúdos inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("\nConteúdos concluídos: " + devJoao.getConteudosConcluidos());
+        System.out.println("\nXP: " + devJoao.calcularTotalXP());
+
+
+
     }
 }
