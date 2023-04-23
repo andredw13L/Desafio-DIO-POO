6fdec33b66d589900ccd25a62ba3f2c61635278e
andredw13L <andreribeiro13l@gmail.com>
2023-04-23 01:43:55 -0300

    Bootcamp, Conteudo e Dev

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,25 +1,9 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Mentoria{
 
-    private String titulo, descricao;
     private int cargaHoraria;
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -29,11 +13,17 @@ public class Curso {
         this.cargaHoraria = cargaHoraria;
     }
 
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
